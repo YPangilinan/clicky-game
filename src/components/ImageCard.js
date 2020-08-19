6dcf89d8f270875343eb6aa3f134cc22dc5d14de
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-08-18 17:03:58 -0700

    adding animations using hooks and installing ghpages npm

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ImageCard(props){
+    const [hovered, setHover] = useState(true);
     return(
-        <section id={props.id} className="tc pa3 hvr-float-shadow animated zoomInUp" value={props.id} onClick={() =>
+        <section id={props.id} className="tc pa3 hvr-float-shadow" value={props.id} onClick={() =>
             props.clickedPlayer(props.id)
             }>
               <article className="w4 pa2 ">
-                <img style = {styleCard} src={props.image} className="db" alt="game-char" />
+                <img style = {styleCard} src={props.image} onMouseEnter = {() => setHover(true)} onMouseOut = {() => setHover(false)} className={hovered ? "animated swing" : "null"} alt="game-char" id= "img"/>
               </article>
         </section>
     )

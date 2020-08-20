684c67695986e538f9a6232e827bb536c314e3b4
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-08-19 17:06:41 -0700

    Updates

---
+++ precache-manifest.9065fc3f17a915855de365f7f9717359.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9babeead57119b4c881c214ba211eb83",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f356751597a0c6cc8494",
+    "url": "/clicky-game/static/css/main.cb5dd175.chunk.css"
+  },
+  {
+    "revision": "1c20456655c256a7dccf",
+    "url": "/clicky-game/static/js/2.271829bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.271829bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f356751597a0c6cc8494",
+    "url": "/clicky-game/static/js/main.bb88ef13.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "9218d0ca511fae32b1ab6cc834683e60",
+    "url": "/clicky-game/static/media/c.9218d0ca.png"
+  },
+  {
+    "revision": "3820e7ff3a67cacbe4942fa907d1c3ab",
+    "url": "/clicky-game/static/media/f.3820e7ff.png"
+  },
+  {
+    "revision": "2af38d5e759955cf396d4f0d56a2ae91",
+    "url": "/clicky-game/static/media/paper.2af38d5e.jpg"
+  }
+]);
